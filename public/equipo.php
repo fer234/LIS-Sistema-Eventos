c0f7f53cc76b801fb17804441142206740320173
karina-angel <lyzzangel97@gmail.com>
2021-03-24 00:50:11 -0600

    seccion about agregada

--- public/equipo.php
+++ public/equipo.php
@@ -9,6 +9,90 @@ include("../master/navbar.php");
 </head>
 <body>
 <!-- Contenido de la pagina -->
+<div class="black white-text center">
+<div class="Container">
+<div class="section">
+<h3>Nuestro Equipo</h3>
+</div>
+</div>
+</div>
+
+<div class="black-text textosindex center-align">
+    <h4>Grand Event</h4>
+    <h5>Experiencia en organización de eventos al servicio de nuestros clientes.</h5>
+</div>
+
+<div class="parrafo">  
+<img class="imgt" src="img/about/FotoJet.jpg">
+<h2 id="titulo">¿Quienes Somos?</h2>
+<p class="txt1">Somos una empresa con más de 5 años de experiencia, dedicada a brindarle el mejor servicio de organización, desde eventos sociales como bodas, fiestas de cumpleaños, graduaciones, hasta eventos ejecutivos como aniversarios de empresa.<br><br> 
+Nuestro deseo es que cada evento realizado sea único e inolvidable, contando con hermosas instalaciones, buena decoración y excelente comida. 
+</p>
+
+</div><br><br>
+
+<center>
+<div class="cont2">
+    <div class="contenido">
+        <img class="img2" src="img/about/p2.jpg">
+        <h2 class="cont">Misión</h2>
+        <p class="mision">Ser los mejores en lo que hacemos, además de brindarle la confianza y seguridad a nuestros clientes de poder organizar el evento de sus sueños con la mejor calidad, principios y valores. </p>
+    </div>
+    <div class="contenido">
+        <img class="img4" src="img/about/p2.jpg">
+        <h2 class="cont1">Visión</h2>
+        <p class="vision"> Ser la mejor opción en organización de eventos dirigida a personas que buscan una experiencia única.<br>
+            Grand Event, reconocido por sus excelentes servicios.</p>
+    </div>
+    <div class="contenido">
+        <img src="img/about/p2.jpg" class="img5">
+        <h2 class="contV">Valores</h2>
+        <p class="valores">-Honestidad<br>
+        -Trabajo en equipo<br>
+        -Calidad<br>
+        -Transparencia<br>
+        -Pasión</p>
+    </div>
+</div>
+</center>
+
+<div class="PTC">
+    <h5>¿Porqué trabajar con nosotros?</h5>
+    <p class="black-text textosindex">Ponemos a su servicio nuestra creatividad y originalidad en la planeación, producción y realización de cualquier tipo de evento que usted desee.</p>
+    <p class="black-text textosindex">Escuchamos sus necesidades y desarrollamos ideas personalizadas a la altura de sus expectativas, para bridarle una experiencia inolvidable.</p>
+    <p class="black-text textosindex">Contamos con un equipo de profesionales altamente capacitados que se encargarán de ofrecerle el mejor de los servicios, además de crear un evento único y adecuado para usted.</p>
+    <p class="black-text textosindex">Somos capaces de dirigir su evento desde el principio hasta el final de manera satisfactoria.</p>
+</div>
+
+<div class="columnas">
+    <h5>Proceso de desarrollo</h5>
+
+    <div class="proceso">
+        <img src="img/planificacion.jfif" class="icono">
+        <div class="caja"></div>
+        <p class="textcolumna">Planificación</p>
+    </div>
+
+    <div class="proceso">
+        <img src="img/organizacion.jpg" class="icono">
+        <div class="caja"></div>
+        <p class="textcolumna">Organización</p>
+    </div>
+
+    <div class="proceso">
+        <img src="img/control.jpg" class="icono">
+        <div class="caja"></div>
+        <p class="textcolumna">Control<br>de calidad</p>
+    </div>
+
+    <div class="proceso">
+        <img src="img/realizacion.jpg" class="icono">
+        <div class="caja"></div>
+        <p class="textcolumna">Realización</p>
+    </div>
+
+</div>
+
 
 <!-- Fin del contenido de la pagina -->
 <?php
